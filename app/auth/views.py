30765293c22495406ebf7479766b8e93e32cab35
景煌 <kiinghom@163.com>
2017-12-02 19:37:23 +0800

    初步

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,14 +2,15 @@ from flask_login import login_user, logout_user, current_user, login_required
 from flask import Blueprint, g, flash, session
 from app import lm, db, app
 
-from flask import  request, redirect, url_for
+from flask import  request, redirect, url_for, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
+from sql_operation import User, login_func, register_func
 
 auth_ = Blueprint('auth', __name__)
 
 @lm.user_loader
 def load_user(id):
-    return #TODO
+    return User.query.filter_by(UserEmail=str(id)).first()
 
 @app.before_request
 def before_request():
@@ -17,14 +18,27 @@ def before_request():
 
 @auth_.route('/register', methods=['POST'])
 def register():
-    return render_template('auth/login.html')
+    ret = register_func(request.form['email'],
+                        request.form['username'],
+                        request.form['password'],
+                        request.form['confirm'])
+    return jsonify({
+        'type': ret
+    })
 
 @auth_.route('/login', methods=['POST'])
 def login():
-    return render_template('login.html')
+    ret, user = login_func(request.form['email'],request.form['password'])
+    if user != None:
+        login_user(user)
+    return jsonify({
+        'type': ret
+    })
 
 @auth_.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return jsonify({
+        'type': 'SUCCEED'
+    })
--- app/photo/views.py
+++ app/photo/views.py
@@ -1,23 +1,58 @@
 from flask_login import login_user, logout_user, current_user, login_required
-from flask import Blueprint, g, flash, session
+from flask import Blueprint, g, flash, session, jsonify
 from app import lm, db, app
-import json
+from sql_operation import *
 
 from flask import  request, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
 photo_ = Blueprint('photo', __name__)
 
-@app.before_request
-def before_request():
-    g.user = current_user
-
 @photo_.route('/upload', methods=['POST'])
+@login_required
 def photo_upload():
-    f = request.files['photo']
     # TODO
+    f = request.files['photo']
+    upload_image(g.user.user_email, bool(request.form['finished']),
+                 f)
     return
 
 @photo_.route('/download', methods=['POST'])
+@login_required
 def photo_download():
-    return jsonify()
+    # TODO
+    return send_my_file(request.form['email'],
+                        bool(request.form['finished']),
+                        request.form['image_name'])
+
+@photo_.route('/release_emoji', methods=['POST'])
+@login_required
+def photo_release_emoji():
+    # TODO
+    release_emoji(g.user.user_email, request.form['image_name'], request.form['category_name'])
+    return jsonify({'type': 'SUCCEED'})
+
+@photo_.route('/get_categories', methods=['POST'])
+@login_required
+def photo_get_categories():
+    return get_categories()
+
+@photo_.route('/get_img_by_category_public', methods=['POST'])
+@login_required
+def photo_get_img_by_category_public():
+    return get_img_by_category_public()
+
+@photo_.route('/get_categories', methods=['POST'])
+@login_required
+def photo_get_categories():
+    return get_categories()
+
+@photo_.route('/get_categories', methods=['POST'])
+@login_required
+def photo_get_categories():
+    return get_categories()
+@photo_.route('/get_categories', methods=['POST'])
+@login_required
+def photo_get_categories():
+    return get_categories()
+
