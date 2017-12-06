dabacf8a08b1acb4132ed9e12399b47b12f5489b
wxlt233 <14307130132@fudan.edu.cn>
2017-12-06 14:04:51 +0800

    Add files via upload

--- app/photo/views.py
+++ app/photo/views.py
@@ -11,50 +11,28 @@ photo_ = Blueprint('photo', __name__)
 @photo_.route('/upload', methods=['POST'])
 @login_required
 def photo_upload():
-    # TODO
-    f = request.files['photo']
-    #文件保存样例代码
-    #filename = f.filename
-    #f.save(os.path.join('/root/daizong/', filename))
-    #下面据此更改
-    upload_image(g.user.user_email, bool(request.form['finished']),
-                 f)
-    return
+    upload_image(g.user.user_email, bool(request.form['finished']),request.form['image_name'],request.form['base64code'])
+    return jsionfy({'type':'SUCCEED'})
 
 @photo_.route('/download', methods=['POST'])
 @login_required
 def photo_download():
-    # TODO
-    # 下载文件样例
-    # from flask import send_from_directory as sfd
-    # sfd(dir, filename)
-    # 下面函数为样例，可以修改
-    return send_my_file(request.form['email'],
-                        bool(request.form['finished']),
-                        request.form['image_name'])
+    return download_image_to_phone(request.form['image_id'])
 
 @photo_.route('/release_emoji', methods=['POST'])
 @login_required
 def photo_release_emoji():
-    # TODO
-    # 根据id修改
-    release_emoji(g.user.user_email, request.form['image_name'], request.form['category_name'])
+    release_emoji(request.form['image_id'], request.form['category_name'])
     return jsonify({'type': 'SUCCEED'})
 
 @photo_.route('/get_categories', methods=['POST'])
 @login_required
 def photo_get_categories():
-    # TODO
-    # 可能要修正一下上传的格式
-    # 比如用jsonify改成json格式
     return get_categories()
 
 @photo_.route('/get_img_by_category_public', methods=['POST'])
 @login_required
 def photo_get_img_by_category_public():
-    # TODO
-    # 不知道戴总要不要同时上传图片
     return get_img_by_category_public(request.form['category_name'],
                                       int(request.form['page']))
-
 
